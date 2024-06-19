0cfb91839c518fd94fdafed3a48fe352a76ec7fe
0x4r45h <0x4r45h@gmail.com>
2024-06-19 21:02:38 +0330

    Add kv node wip

--- init.sh
+++ init.sh
@@ -8,14 +8,36 @@ wget https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json -O
 #Then verify the correctness of the genesis configuration file:
 0gchaind validate-genesis
 
+### app.toml changes
+# Enable api
+sed -i '/^\[api\]/,/^\[/{s/^enable = .*/enable = true/}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[api\]/,/^\[/{s/^swagger = .*/swagger = true/}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[api\]/,/^\[/{s|^address = .*|address = "tcp://0.0.0.0:1317"|}' $HOME/.0gchain/config/app.toml
 
-sed -i "s#seeds = \".*\"#seeds = \"$SEEDS\"#g" $HOME/.0gchain/config/config.toml
+# Enable grpc
+sed -i '/^\[grpc\]/,/^\[/{s/^enable = .*/enable = true/}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[grpc\]/,/^\[/{s|^address = .*|address = "0.0.0.0:9090"|}' $HOME/.0gchain/config/app.toml
 
-## configure  node to advertise host public ip to outside peers if provided
-echo "Advertising public ip $EXTERNAL_IP"
-sed -i "s#external_address = \".*\"#external_address = \"$EXTERNAL_IP:$P2P_PORT\"#g" $HOME/.0gchain/config/config.toml
-sed -i "s#laddr = \"tcp://.*:26657\"#laddr = \"tcp://0.0.0.0:26657\"#g" $HOME/.0gchain/config/config.toml
-#set gas price
-sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
-# enable index
-sed -i "s/^indexer *=.*/indexer = \"kv\"/" $HOME/.0gchain/config/config.toml
+# Enable grpc-web
+sed -i '/^\[grpc-web\]/,/^\[/{s/^enable = .*/enable = true/}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[grpc-web\]/,/^\[/{s|^address = .*|address = "0.0.0.0:9091"|}' $HOME/.0gchain/config/app.toml
+
+# Enable json-rpc
+sed -i '/^\[json-rpc\]/,/^\[/{s/^enable = .*/enable = true/}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[json-rpc\]/,/^\[/{s|^address = .*|address = "0.0.0.0:8545"|}' $HOME/.0gchain/config/app.toml
+sed -i '/^\[json-rpc\]/,/^\[/{s|^ws-address = .*|ws-address = "0.0.0.0:8546"|}' $HOME/.0gchain/config/app.toml
+
+
+
+### config.toml changes
+
+sed -i "s|^proxy_app *=.*|proxy_app = \"tcp://0.0.0.0:26658\"|" $HOME/.0gchain/config/config.toml
+sed -i "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/.0gchain/config/config.toml
+# enable indexer
+sed -i "s|^indexer *=.*|indexer = \"kv\"|" $HOME/.0gchain/config/config.toml
+# configure  node to advertise host public ip to outside peers if provided
+sed -i "s|^external_address *=.*|external_address = \"$EXTERNAL_IP:$P2P_PORT\"|" $HOME/.0gchain/config/config.toml
+# set RPC listen address
+sed -i '/^\[rpc\]/,/^\[/{s|^laddr = .*|laddr = "tcp://0.0.0.0:26657"|}' $HOME/.0gchain/config/app.toml
+# set p2p2 listen address
+sed -i '/^\[p2p\]/,/^\[/{s|^laddr = .*|laddr = "tcp://0.0.0.0:26656"|}' $HOME/.0gchain/config/app.toml
