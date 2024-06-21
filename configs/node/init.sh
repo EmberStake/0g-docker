fab29c90241e78b761ff8387f2fe47cdd1073f20
0x4r45h <0x4r45h@gmail.com>
2024-06-21 20:51:06 +0330

    update genesis url to v0.2.3

--- configs/node/init.sh
+++ configs/node/init.sh
@@ -4,7 +4,7 @@
 0gchaind config keyring-backend test
 0gchaind init $MONIKER --chain-id $CHAIN_ID
 
-wget https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json -O $HOME/.0gchain/config/genesis.json
+wget https://github.com/0glabs/0g-chain/releases/download/v0.2.3/genesis.json -O $HOME/.0gchain/config/genesis.json
 #Then verify the correctness of the genesis configuration file:
 0gchaind validate-genesis
 
