fdb3c7b4a5e563b1a503906ecf1416bfc00f3f0d
0x4r45h <0x4r45h@gmail.com>
2025-07-26 18:24:01 +0330

    add galileo node & geth

--- configs/node/init.sh
+++ configs/node/init.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-0gchaind config chain-id $CHAIN_ID
-0gchaind config keyring-backend test
-0gchaind init $MONIKER --chain-id $CHAIN_ID
+#0gchaind config chain-id $CHAIN_ID
+#0gchaind config keyring-backend test
+0gchaind init $MONIKER  --home /tmp/0gchaind
 
-wget https://github.com/0glabs/0g-chain/releases/download/v0.2.3/genesis.json -O $HOME/.0gchain/config/genesis.json
-#Then verify the correctness of the genesis configuration file:
-0gchaind validate-genesis
+
+cp /tmp/0gchaind/data/priv_validator_state.json $DAEMON_HOME/data/
+cp /tmp/0gchaind/config/node_key.json $DAEMON_HOME/config/
+cp /tmp/0gchaind/config/priv_validator_key.json $DAEMON_HOME/config/
+
 
 ### app.toml changes
 # it is not possible to modify below configs through cli flags, so we set their default value here
-sed -i '/^\[api\]/,/^\[/{s/^swagger = .*/swagger = true/}' $HOME/.0gchain/config/app.toml
-sed -i '/^\[api\]/,/^\[/{s|^address = .*|address = "tcp://0.0.0.0:1317"|}' $HOME/.0gchain/config/app.toml
+#sed -i '/^\[api\]/,/^\[/{s/^swagger = .*/swagger = true/}' $DAEMON_HOME/config/app.toml
+#sed -i '/^\[api\]/,/^\[/{s|^address = .*|address = "tcp://0.0.0.0:1317"|}' $DAEMON_HOME/config/app.toml
+sed -i '/^\[beacon-kit.engine\]/,/^\[/{s|^rpc-dial-url = .*|rpc-dial-url = "http://geth:8551"|}' $DAEMON_HOME/config/app.toml
 
 
 ### config.toml changes
-sed -i "s|^indexer *=.*|indexer = \"null\"|" $HOME/.0gchain/config/config.toml
+#sed -i "s|^indexer *=.*|indexer = \"null\"|" $DAEMON_HOME/config/config.toml
