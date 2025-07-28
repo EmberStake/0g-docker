7693b90c7c3ddc6ce29095acfa92ff0d9df1ade3
0x4r45h <0x4r45h@gmail.com>
2025-07-28 14:47:07 +0330

    update readme and fix bugs

--- configs/node/init.sh
+++ configs/node/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#0gchaind config chain-id $CHAIN_ID
-#0gchaind config keyring-backend test
 0gchaind init $MONIKER  --home /tmp/0gchaind
 
 
@@ -9,13 +7,3 @@ cp /tmp/0gchaind/data/priv_validator_state.json $DAEMON_HOME/data/
 cp /tmp/0gchaind/config/node_key.json $DAEMON_HOME/config/
 cp /tmp/0gchaind/config/priv_validator_key.json $DAEMON_HOME/config/
 
-
-### app.toml changes
-# it is not possible to modify below configs through cli flags, so we set their default value here
-#sed -i '/^\[api\]/,/^\[/{s/^swagger = .*/swagger = true/}' $DAEMON_HOME/config/app.toml
-#sed -i '/^\[api\]/,/^\[/{s|^address = .*|address = "tcp://0.0.0.0:1317"|}' $DAEMON_HOME/config/app.toml
-sed -i '/^\[beacon-kit.engine\]/,/^\[/{s|^rpc-dial-url = .*|rpc-dial-url = "http://geth:8551"|}' $DAEMON_HOME/config/app.toml
-
-
-### config.toml changes
-#sed -i "s|^indexer *=.*|indexer = \"null\"|" $DAEMON_HOME/config/config.toml
